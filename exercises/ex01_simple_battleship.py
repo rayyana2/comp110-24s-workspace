033fecd034fbab8c3f83cd535ab532661267bf32
Rayyan Arshad <rayyanaarshad@gmail.com>
2024-01-19 19:31:23 -0500

    prompts working

--- exercises/ex01_simple_battleship.py
+++ exercises/ex01_simple_battleship.py
@@ -3,7 +3,17 @@
 __author__ = "730656243"
 
 user_input: str = input("Pick a secret boat location between 1 and 4:")
-ship_number: int = int(user_input)
+ship_number_p1: int = int(user_input)
 
-if ship_number < 1 or ship_number > 4:
-    print(f"Error! {ship_number} too low!")
+if ship_number_p1 < 1:
+    print(f"Error! {ship_number_p1} too low!")
+if ship_number_p1 > 4:
+    print(f"Error! {ship_number_p1} too high!")
+
+user_input2: str = input("Pick a secret boat loacation between 1 and 4:")
+ship_number_p2: int = int(user_input2)
+
+if ship_number_p2 < 1:
+    print(f"Error! {ship_number_p2} too low!")
+if ship_number_p2 > 4:
+    print(f"Error! {ship_number_p2} too high!")
